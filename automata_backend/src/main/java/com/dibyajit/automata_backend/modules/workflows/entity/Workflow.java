116a72eb579e56cd6164202c07e1fed0be657218
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-06-02 23:35:38 +0530

    first commit

---
+++ automata_backend/src/main/java/com/dibyajit/automata_backend/modules/workflows/entity/Workflow.java
@@ -0,0 +1,16 @@
+package com.dibyajit.automata_backend.modules.workflows.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "workflows")
+@Data
+@Getter
+@Setter
+public class Workflow {
+
+}
